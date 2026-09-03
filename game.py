51e7a76c2346b65e1bfcc1c5e10d29eda16dd76b
Osmar Farfan <osmaravila@gmail.com>
2026-09-03 20:35:15 -0300

    Agregar data_loader.py y actualizar clase Game con rank y global_sales

--- game.py
+++ game.py
@@ -1,23 +1,20 @@
 class Game:
-    def __init__(self, name, year, genre, publisher, platform, sales):
+    def __init__(self, rank, name, year, genre, publisher, platform, sales, global_sales):
     
         if isinstance(year,str) and year.isdigit() and len(year) == 4 and 1900 <= int(year) <= 2026 :
             self.year = int(year)
         else:
             self.year =  None
-            
+        self.rank = rank   
         self.name = name
         self.genre = genre
         self.publisher = publisher
         self.platform = platform
         self.sales = sales
+        self.global_sales = global_sales
         
     def __str__ (self):
-       return f"Nombre:{self.name}\nAño de lanzamiento:{self.year}\nGenero:{self.genre}\nPublicadores:{self.publisher}\nPlataforma:{self.platform}\nVentas{self.sales}"
+       return f"Puesto:{self.rank}\nNombre:{self.name}\nAño de lanzamiento:{self.year}\nGenero:{self.genre}\nPublicadores:{self.publisher}\nPlataforma:{self.platform}\nVentas por region{self.sales}\nVentas Globales{self.global_sales}"
         
         
-        
-        
-minecraft = Game("Minecraft","10","Sandbox","Microsoft","PC",(300,200,100,120))
-
-print(minecraft)
+    
