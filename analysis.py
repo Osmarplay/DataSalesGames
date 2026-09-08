9b5733701c6ad275e6fb841bb2430bd07f277024
Osmar Farfan <osmaravila@gmail.com>
2026-09-08 12:01:36 -0300

    Implementar publisher_most_sold

--- analysis.py
+++ analysis.py
@@ -35,3 +35,22 @@ def top_game_per_genre(games):
 
     return top_game_list
 
+def publisher_most_sold(games):
+    
+    publishers = {}
+
+    for game in games:
+        if not game.publisher in publishers:
+            publishers[game.publisher] = []
+        publishers[game.publisher].append(game)
+        
+    publishers_sales = {}
+
+    for publisher, game_list in publishers.items():
+        total_ventas = sum(game.global_sales for game in game_list)
+        publishers_sales[publisher] = total_ventas
+        
+    publisher_most_sales = max(publishers_sales, key=publishers_sales.get)
+    ventas_totales = publishers_sales[publisher_most_sales]
+    
+    return publisher_most_sales, ventas_totales
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from data_loader import load_games
-from analysis import genre_most_sold, top_game_per_genre
+from analysis import genre_most_sold, top_game_per_genre, publisher_most_sold
 
 
 if __name__ == "__main__":
@@ -14,3 +14,5 @@ print(f"\nJuego con más ventas por género:")
 for game in top_games:
     print(f" {game.genre}: {game.name}, ({round(game.global_sales)}M ventas)")
        
+publisher, ventas_pub = publisher_most_sold(games)
+print(f"\nPublisher con más ventas: {publisher} con {round(ventas_pub, 2)}M ventas globales")
