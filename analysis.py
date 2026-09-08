f0f649870c4f0352669507608465a2c75c9dca21
Osmar Farfan <osmaravila@gmail.com>
2026-09-08 11:12:44 -0300

    Implementar top_game_per_genre y redondear valores de ventas

--- analysis.py
+++ analysis.py
@@ -1,4 +1,3 @@
-
 def genre_most_sold(games):
     
     genero = {}
@@ -17,8 +16,22 @@ def genre_most_sold(games):
     genero_mas_vendido = max(genero_ventas, key=genero_ventas.get)
     ventas_totales = genero_ventas[genero_mas_vendido]
     
-    
     return genero_mas_vendido, ventas_totales
 
+def top_game_per_genre(games):
+    
+    genero = {}
+    
+    for game in games:
+        if not game.genre in genero:
+            genero[game.genre] = []
+        genero[game.genre].append(game)
+            
+    top_game_list = []
+            
+    for genre in genero:
+        mejor_juego = max(genero[genre], key=lambda game: game.global_sales)
+        top_game_list.append(mejor_juego)
 
+    return top_game_list
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from data_loader import load_games
-from analysis import genre_most_sold
+from analysis import genre_most_sold, top_game_per_genre
+
 
 if __name__ == "__main__":
     games = load_games("vgsales.csv")
@@ -7,3 +8,9 @@ if __name__ == "__main__":
     
     genero, ventas = genre_most_sold(games)
     print(f"Género más vendido: {genero} con {ventas} ventas globales")
+    
+top_games = top_game_per_genre(games)
+print(f"\nJuego con más ventas por género:")
+for game in top_games:
+    print(f" {game.genre}: {game.name}, ({round(game.global_sales)}M ventas)")
+       
